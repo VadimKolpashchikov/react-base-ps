fc882f983861eb9a14526abe7bb6397dcd5fcbbb
VadimKolpashchikov <5vadims55.555@gmail.com>
2026-09-20 16:14:35 +0300

    add scss preprocessor, add fonts and normalize.css

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,25 @@
 import react, { reactCompilerPreset } from '@vitejs/plugin-react';
 import babel from '@rolldown/plugin-babel';
 import { defineConfig } from 'vite';
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
   base: './',
+  resolve: {
+    alias: {
+      '@': path.resolve(import.meta.dirname, './src'),
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `
+          @use "sass:color";
+          @use "@/styles/_variables.scss" as *;
+        `,
+      },
+    },
+  },
   plugins: [react(), babel({ presets: [reactCompilerPreset()] })],
 });
